9c0cae0a569ab967c8b8cf1fd564dfa8f21e813c
Shipon Roy <shiponworks@gmail.com>
2026-02-12 06:32:37 +0600

    Completes: video task 5

--- 0-5 Why Props Are ReadOnly in React Learn With Patterns/src/Components/Child.jsx
+++ 0-5 Why Props Are ReadOnly in React Learn With Patterns/src/Components/Child.jsx
@@ -1,5 +1,35 @@
-const Child = () => {
-  return <div>Child</div>;
+const Child = ({ counts, setCount, onDecrement, onReset }) => {
+  return (
+    <div className="text-center py-40 flex gap-4 items-center flex-col">
+      <h1>
+        <span>Count: </span>
+        {counts}
+      </h1>
+
+      <div className="flex gap-4">
+        <button
+          className="p-3 bg-pink-400 rounded-lg font-bold text-white"
+          onClick={setCount}
+        >
+          Increment
+        </button>
+
+        <button
+          className="p-3 bg-red-400 rounded-lg font-bold text-white"
+          onClick={onDecrement}
+        >
+          Decrement
+        </button>
+      </div>
+
+      <button
+        className="p-3 bg-violet-400 rounded-lg font-bold text-white"
+        onClick={onReset}
+      >
+        Reset
+      </button>
+    </div>
+  );
 };
 
 export default Child;
--- 0-5 Why Props Are ReadOnly in React Learn With Patterns/src/Components/Parent.jsx
+++ 0-5 Why Props Are ReadOnly in React Learn With Patterns/src/Components/Parent.jsx
@@ -1,9 +1,21 @@
+import { useState } from 'react';
 import Child from './Child';
 
 const Parent = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => setCount(count + 1);
+  const Decrement = () => setCount(count - 1);
+  const reset = () => setCount(0);
+
   return (
     <div>
-      <Child />
+      <Child
+        counts={count}
+        setCount={incrementCount}
+        onDecrement={Decrement}
+        onReset={reset}
+      />
     </div>
   );
 };
