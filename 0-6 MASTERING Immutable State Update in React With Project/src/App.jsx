3ba3122eccc3a8dfa1b73d876f6233046f5a6757
Shipon Roy <shiponworks@gmail.com>
2026-02-13 08:34:28 +0600

    Completetes: userlist.jsx pratce

--- 0-6 MASTERING Immutable State Update in React With Project/src/App.jsx
+++ 0-6 MASTERING Immutable State Update in React With Project/src/App.jsx
@@ -1,5 +1,11 @@
+import UserList from './Immutability/UserList';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <UserList />
+    </div>
+  );
 };
 
 export default App;
